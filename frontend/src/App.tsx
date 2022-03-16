91403f9a4b7176d150dfb1c8051694867255b4fd
okdongdong <kjch6411@naver.com>
2022-03-16 11:35:20 +0900

    #S06P22E201-48 feat TypeScript 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import SignIn from "./components/Accounts/FormBackground";
 
 function App() {
   return (
     <div className="App">
-      <SignIn></SignIn>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
