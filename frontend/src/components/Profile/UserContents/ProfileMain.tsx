ab3f97e549c167032cfb95ac153d160a983e1191
hojeong <jhj20071@naver.com>
2022-03-22 18:17:48 +0900

    #S06P22E201-94 feat profileContent 초안

--- frontend/src/components/Profile/UserContents/ProfileMain.tsx
+++ frontend/src/components/Profile/UserContents/ProfileMain.tsx
@@ -1,16 +1,27 @@
-// import React from "react";
-// import { makeStyles } from "@material-ui/core/styles";
-// import LikePlace from "./LikePlace";
-// import LikeFullCourse from "./LikeFullCourse";
-// import MyFullCourse from "./MyFullCourse";
-// import VisitedPlace from "./VisitedPlace";
-// function ProfileMain() {
-//   return (
-//     <div>
-//       <LikePlace></LikePlace>,<MyFullCourse></MyFullCourse>,
-//     <LikeFullCourse></LikeFullCourse>,<VisitedPlace></VisitedPlace>
-//     </div>
-//   );
-// }
-// export default ProfileMain;
+import React from "react";
+import LikePlace from "./LikePlace";
+import LikeFullCourse from "./LikeFullCourse";
+import MyFullCourse from "./MyFullCourse";
+import VisitedPlace from "./VisitedPlace";
+import { Theme } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+const useStyles = makeStyles((theme: Theme) => ({
+  likePlace: {
+    marginTop: theme.spacing(12),
+    // marginRight: theme.spacing(),
+  },
+}));
+function ProfileMain() {
+  const classes = useStyles();
+  return (
+    <div>
+      <div className={classes.likePlace}>
+        <LikePlace></LikePlace>,
+      </div>
+      <MyFullCourse></MyFullCourse>,<LikeFullCourse></LikeFullCourse>,
+      {/* <VisitedPlace></VisitedPlace> */}
+    </div>
+  );
+}
+export default ProfileMain;
 export {};
--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,9 @@
 import ProfileHeader from "../components/Profile/UserInfo/ProfileHeader";
-// import ProfileMain from "../components/Profile/UserContents/ProfileMain";
+import ProfileMain from "../components/Profile/UserContents/ProfileMain";
 function Profile() {
   return (
     <div>
-      <ProfileHeader></ProfileHeader>,{/* <ProfileMain></ProfileMain> */}
+      <ProfileHeader></ProfileHeader>,<ProfileMain></ProfileMain>
     </div>
   );
 }
