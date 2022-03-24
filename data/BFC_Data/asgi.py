1fc4a3bd9b53f4d5fa5e82dddbeaf81a16d7f958
xogxog <nasa0939@naver.com>
2022-03-24 15:06:16 +0900

    #S06P22E201-97 feat DjangoSetting

---
+++ data/BFC_Data/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for BFC_Data project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BFC_Data.settings')
+
+application = get_asgi_application()
