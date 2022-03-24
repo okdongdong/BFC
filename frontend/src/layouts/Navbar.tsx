d4b3f0d9cccc6878299b1e8fdb2be5971db5fe2f
okdongdong <kjch6411@naver.com>
2022-03-24 14:53:39 +0900

    #S06P22E201-121 feat 로그인시 네비바 전환

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -1,43 +1,73 @@
 import { alpha, AppBar, Box, styled, Toolbar } from "@mui/material";
-import Logo from "../components/logo";
-import NavbarText from "../components/navbarText";
+import { connect } from "react-redux";
+import { AccountReducer } from "../redux/rootReducer";
+import Logo from "../components/Logo";
+import NavbarText from "../components/NavbarText";
+import Profileimg from "../components/Profile/UserInfo/ProfileImg";
 
 // 헤더 화면 (상단 메뉴바)
 
-const Navbar = () => {
-  const APPBAR_MOBILE = 64;
-  const APPBAR_DESKTOP = 92;
+const APPBAR_MOBILE = 64;
+const APPBAR_DESKTOP = 92;
 
-  const RootStyle = styled(AppBar)(({ theme }) => ({
-    boxShadow: "none",
-    backdropFilter: "blur(6px)",
-    WebkitBackdropFilter: "blur(6px)",
-    backgroundColor: alpha(theme.palette.background.default, 0.72),
-  }));
+const RootStyle = styled(AppBar)(({ theme }) => ({
+  boxShadow: "none",
+  backdropFilter: "blur(6px)",
+  WebkitBackdropFilter: "blur(6px)",
+  backgroundColor: alpha(theme.palette.background.default, 0.72),
+}));
 
-  const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
-    minHeight: APPBAR_MOBILE,
-    [theme.breakpoints.up("lg")]: {
-      minHeight: APPBAR_DESKTOP,
-      padding: theme.spacing(0, 5),
-    },
-  }));
+const ToolbarStyle = styled(Toolbar)(({ theme }) => ({
+  minHeight: APPBAR_MOBILE,
+  [theme.breakpoints.up("lg")]: {
+    minHeight: APPBAR_DESKTOP,
+    padding: theme.spacing(0, 5),
+  },
+}));
 
+const Navbar = ({ isLogin, nickname, profileImg }: Props) => {
   return (
     <RootStyle>
       <ToolbarStyle>
         <Box sx={{ flexGrow: 1 }} />
         <Logo />
-        <NavbarText to="/attraction" text="여행지"></NavbarText>
-        <NavbarText to="/restaurant" text="맛집"></NavbarText>
-        <NavbarText to="/info" text="이용방법"></NavbarText>
-        <Box sx={{ flexGrow: 1 }} />
-        <NavbarText to="/signup" text="회원가입"></NavbarText>
-        <NavbarText to="/login" text="로그인"></NavbarText>
+        <NavbarText to="attraction" text="여행지"></NavbarText>
+        <NavbarText to="restaurant" text="맛집"></NavbarText>
+        <NavbarText to="info" text="이용방법"></NavbarText>
+        <Box sx={{ flexGrow: 2 }} />
+
+        {isLogin ? (
+          <Box sx={{ display: "flex" }}>
+            <Box sx={{ height: 64 }}>
+              <img
+                src={profileImg}
+                alt="profileImg"
+                style={{ width: 64, height: 64, borderRadius: "50%" }}
+              />
+            </Box>
+            <NavbarText to="profile" text={nickname}></NavbarText>
+          </Box>
+        ) : (
+          <Box sx={{ display: "flex" }}>
+            <NavbarText to="signup" text="회원가입"></NavbarText>
+            <NavbarText to="login" text="로그인"></NavbarText>
+          </Box>
+        )}
         <Box sx={{ flexGrow: 1 }} />
       </ToolbarStyle>
     </RootStyle>
   );
 };
 
-export default Navbar;
+const mapStateToProps = ({ account }: AccountReducer) => ({
+  isLogin: account.isLogin,
+  nickname: account.nickname,
+  profileImg: account.profileImg,
+});
+
+const mapDispatchToProps = {};
+
+type Props = ReturnType<typeof mapStateToProps>;
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
+
