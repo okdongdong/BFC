aedc673d12688971017e38bc34d13a1d04bff006
양지훈 <kelvin9149@gmail.com>
2022-03-29 04:05:58 +0900

    #S06P22E201-133 feat 스케줄 수정(D&D)

--- backend/src/main/java/com/busanfullcourse/bfc/db/repository/ScheduleRepository.java
+++ backend/src/main/java/com/busanfullcourse/bfc/db/repository/ScheduleRepository.java
@@ -19,8 +19,20 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @EntityGraph(attributePaths = {"place"})
     List<Schedule> findTop4ByFullCourseAndPlaceIsNotNullAndPlaceThumbnailIsNotNull(FullCourse fullCourse);
 
-    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThan(Long fullCourseId, Integer day, Integer seq);
+    // 특정 day에서, seq보다 큰
+    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThan(
+            Long fullCourseId, Integer day, Integer seq);
 
-    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThanEqual(Long fullCourseId, Integer day, Integer seq);
+    // 특정 day에서, seq보다 크거나 같은
+    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThanEqual(
+            Long fullCourseId, Integer day, Integer seq);
+
+    // 특정 day에서, seqA보다 크고 seqB보다 작거나 같은
+    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThanAndSequenceLessThanEqual(
+            Long fullCourseId, Integer day, Integer seqA, Integer seqB);
+
+    // 특정 day에서, seqA보다 크거나 같고 seqB보다 작은
+    List<Schedule> findSchedulesByFullCourseFullCourseIdAndDayAndSequenceGreaterThanEqualAndSequenceLessThan(
+            Long fullCourseId, Integer day, Integer seqA, Integer seqB);
 
 }
