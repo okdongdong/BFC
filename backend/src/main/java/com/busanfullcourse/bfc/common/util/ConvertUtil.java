e0e3413bae73a18131eaedfcd667a9adab536245
jsw3788 <jsw3788@naver.com>
2022-03-30 16:20:06 +0900

    #S06P22E201-00 fix 프로필 이미지 변환 수정

--- backend/src/main/java/com/busanfullcourse/bfc/common/util/ConvertUtil.java
+++ backend/src/main/java/com/busanfullcourse/bfc/common/util/ConvertUtil.java
@@ -1,5 +1,7 @@
 package com.busanfullcourse.bfc.common.util;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +16,7 @@ public class ConvertUtil {
         for (Byte b: bytes) {
             primitiveBytes[j++] = b;
         }
-        return new String(primitiveBytes);
+        return "data:image/png;base64," +
+                StringUtils.newStringUtf8(Base64.encodeBase64(primitiveBytes, false));
     }
 }
