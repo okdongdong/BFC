5b1bd0421b0f9764047e2e433773efd83db7acf3
양지훈 <kelvin9149@gmail.com>
2022-03-30 15:41:46 +0900

    #S06P22E201-137 feat 개인 풀코스 리스트 조회

--- backend/src/main/java/com/busanfullcourse/bfc/api/service/FullCourseService.java
+++ backend/src/main/java/com/busanfullcourse/bfc/api/service/FullCourseService.java
@@ -142,7 +142,25 @@ public class FullCourseService {
                 .title(like.getFullCourse().getTitle())
                 .startedOn(like.getFullCourse().getStartedOn())
                 .finishedOn(like.getFullCourse().getFinishedOn())
-                .thumbnailList(FullCourseListRes.ofThumbnailList(scheduleRepository.findTop4ByFullCourseFullCourseIdAndPlaceIsNotNullAndPlaceThumbnailIsNotNull(like.getFullCourse().getFullCourseId())))
+                .thumbnailList(FullCourseListRes.ofThumbnailList(
+                        scheduleRepository.findTop4ByFullCourseFullCourseIdAndPlaceIsNotNullAndPlaceThumbnailIsNotNull(
+                                like.getFullCourse().getFullCourseId())))
+                .build());
+    }
+
+    public Page<FullCourseListRes> getMoreUserFullCourse(Long userId, Pageable pageable) {
+        User user = userRepository.getById(userId);
+        Page<FullCourse> page = fullCourseRepository.findAllByUserOrderByStartedOn(user, pageable);
+
+        return page.map(fullCourse -> FullCourseListRes.builder()
+                .fullCourseId(fullCourse.getFullCourseId())
+                .likeCnt(fullCourse.getLikeCnt())
+                .title(fullCourse.getTitle())
+                .startedOn(fullCourse.getStartedOn())
+                .finishedOn(fullCourse.getFinishedOn())
+                .thumbnailList(FullCourseListRes.ofThumbnailList(
+                        scheduleRepository.findTop4ByFullCourseFullCourseIdAndPlaceIsNotNullAndPlaceThumbnailIsNotNull(
+                                fullCourse.getFullCourseId())))
                 .build());
     }
 }
