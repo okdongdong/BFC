d687f35efd86b07a890705d2b4f1eb3dc4a58745
jsw3788 <jsw3788@naver.com>
2022-03-31 14:48:50 +0900

    #S06P22E201-139 feat 애플리케이션 시작시 엘라스틱 서치에 적재하고 종료시 데이터를 내리는 빈 등록

--- backend/src/main/java/com/busanfullcourse/bfc/db/entity/Place.java
+++ backend/src/main/java/com/busanfullcourse/bfc/db/entity/Place.java
@@ -1,6 +1,7 @@
 package com.busanfullcourse.bfc.db.entity;
 
 import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -30,31 +31,30 @@ public class Place {
     @Field(type = FieldType.Long)
     private Long placeId;
 
-    @Field(type=FieldType.Keyword)
+    @Field(type = FieldType.Keyword)
     private String name;
 
-    @Transient
+    @Field(type = FieldType.Text)
     private String info;
 
-    @Column(name="open_time")
+    @Column(name = "open_time")
     private String openTime;
 
     private Float lat;
 
     private Float lng;
 
-    @Transient
+    @Field(type = FieldType.Text)
     private String address;
 
     private Boolean category;
 
-    @Transient
     private String phone;
 
     @Field(type = FieldType.Keyword)
     private String label;
 
-    @Transient
+    @Field(type = FieldType.Text)
     private String station;
 
     @Column(name = "average_score")
@@ -64,8 +64,8 @@ public class Place {
     @Column(name = "score_count")
     private Integer scoreCount = 0;
 
-    @Transient
-    private String thumbnail;
+
+    private String thumbnail = "";
 
     @Transient
     @OneToMany(mappedBy = "place", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -78,13 +78,18 @@ public class Place {
 
 
     @PersistenceConstructor
-    public Place(Long placeId, String name, Float lat, Float lng, Boolean category, String label, String location) {
+    public Place(Long placeId, String name, String info, String address, String station, String phone, @Value("#root.thumbnail ?: ''") String thumbnail, Float lat, Float lng, Boolean category, String label) {
         this.placeId = placeId;
         this.name = name;
         this.lat = lat;
         this.lng = lng;
         this.category = category;
         this.label = label;
+        this.info = info;
+        this.address = address;
+        this.station = station;
+        this.phone = phone;
+        this.thumbnail = thumbnail;
         this.location = this.lat.toString() + ", " + this.lng.toString();
     }
 }
