41ef01f38251c6b67cbe20d7a3cebacd63c287e4
양지훈 <kelvin9149@gmail.com>
2022-04-01 14:43:03 +0900

    #S06P22E201-132 feat 공유메일 전송

--- backend/src/main/java/com/busanfullcourse/bfc/api/controller/EmailController.java
+++ backend/src/main/java/com/busanfullcourse/bfc/api/controller/EmailController.java
@@ -3,11 +3,9 @@ package com.busanfullcourse.bfc.api.controller;
 import com.busanfullcourse.bfc.api.response.EmailAuthRes;
 import com.busanfullcourse.bfc.api.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -24,5 +22,21 @@ public class EmailController {
             Map<String, String> email
     ) throws Exception {
         return ResponseEntity.ok(emailService.sendCode(email.get("email")));
+    }
+
+    @PostMapping("/verification/reset")
+    public ResponseEntity<EmailAuthRes> emailPasswordAuth(
+            @RequestBody
+            Map<String, String> email
+    ) throws Exception {
+        return ResponseEntity.ok(emailService.sendResetCode(email.get("email")));
+    }
+
+    @PostMapping("/{fullCourseId}/share")
+    public ResponseEntity<String> shareFullCourse(
+            @PathVariable Long fullCourseId,
+            @RequestBody Map<String, String> invitedUser) throws IllegalAccessException {
+        emailService.shareFullCourse(fullCourseId, invitedUser);
+        return ResponseEntity.ok("초대메일이 발송되었습니다.");
     }
 }
