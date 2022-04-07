876ea1e3010063d4e61c4fcbe54399eb96c6fb15
양지훈 <kelvin9149@gmail.com>
2022-04-08 04:42:56 +0900

    #S06P22E201-00 fix syntax error 수정

--- backend/src/main/java/com/busanfullcourse/bfc/api/service/EmailService.java
+++ backend/src/main/java/com/busanfullcourse/bfc/api/service/EmailService.java
@@ -103,15 +103,18 @@ public class EmailService {
         for (int i = 0; i < 16; i++) {
             int rIndex = rnd.nextInt(3);
             switch (rIndex) {
-                case 0 ->
-                        // a-z
-                        temp.append((char) ((int) (rnd.nextInt(26)) + 97));
-                case 1 ->
-                        // A-Z
-                        temp.append((char) ((int) (rnd.nextInt(26)) + 65));
-                case 2 ->
-                        // 0-9
-                        temp.append((rnd.nextInt(10)));
+                case 0:
+                    // a-z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 97));
+                    break;
+                case 1:
+                    // A-Z
+                    temp.append((char) ((int) (rnd.nextInt(26)) + 65));
+                    break;
+                case 2:
+                    // 0-9
+                    temp.append((rnd.nextInt(10)));
+                    break;
             }
         }
         return temp.toString();
